42e019f4e1ae7bb89b5a6bfae03c365c6682e9c3
RFatih <39864827+R-Fatih@users.noreply.github.com>
2024-12-26 21:38:12 +0300

    up

--- AcunMedyaPortfolioProject/Controllers/CategoryController.cs
+++ AcunMedyaPortfolioProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcunMedyaPortfolioProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,33 @@ namespace AcunMedyaPortfolioProject.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+
+        PortfolioDBEntities1 db = new PortfolioDBEntities1();
         public ActionResult CategoryList()
         {
+            var categoryList = db.Categories.ToList();
+            return View(categoryList);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
         }
     }
 }
